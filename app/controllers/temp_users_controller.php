950b2aafb621479909043cf196d696435fff6bc2
ngiorgi <ngiorgi@56ec8b77-ce45-43ce-8f5e-74b29490009c>
2009-11-22 16:35:06 +0000

    temp user work

--- app/controllers/temp_users_controller.php
+++ app/controllers/temp_users_controller.php
@@ -1,10 +1,30 @@
 <?php
 class TempUsersController extends AppController {
-      var $name = 'TempUsers';
-      var $scaffold;
-      var $components = array('Auth');
-      //var $uses = array('User');
+	var $name = 'TempUsers';
+	var $scaffold;
+	var $components = array('Auth');
+	var $helpers = array('Time');
 
+	function beforeFilter() {
+      
+        //list of actions that do not need authentication
+		$this->Auth->allow('userCleanUp');
+        
+	
+        
+    }
+      
+	//this function will delete TempUser accounts which have not been activated
+	function userCleanUp() {
+		//disable need for view
+		//$this->autoRender = false;
+		
+		//$findTemp = $this->TempUser->find('all',array('conditions' => array( 
+		
+		
+		$checkTime = $time->gmt();
+		$this->set('checkTime',$checkTime);
+	}
       
 }
 ?>
