b4ad6a154949e554297f5034ffee723befb6dbd5
wais <wais@56ec8b77-ce45-43ce-8f5e-74b29490009c>
2010-01-11 01:44:27 +0000

    Added extra fields to dummy data.

--- db/scripts/create_data.sql
+++ db/scripts/create_data.sql
@@ -4,14 +4,22 @@ INSERT INTO users (email, password, username) VALUES ("scott@studydeck.com", "59
 INSERT INTO users (email, password, username) VALUES ("nicolo@studydeck.com", "5911211b41ad0fc56b09e05fe73ee5cedd42ca23", "nicolo");
 
 -- CREATE DECK
-INSERT INTO decks (deck_name, privacy, view_count, user_id) VALUES ("EMC Vocabulary", 1, 0, 1);
-INSERT INTO decks (deck_name, privacy, view_count, user_id) VALUES ("State Capitals", 1, 0, 2);
-INSERT INTO decks (deck_name, privacy, view_count, user_id) VALUES ("SAT Words", 1, 0, 3);
+INSERT INTO decks (deck_name, privacy, view_count, user_id, description, created) VALUES ("SAN Terms", 1, 0, 1, "Storage Area Networks terms and definitions.", NOW());
+INSERT INTO decks (deck_name, privacy, view_count, user_id, description, created) VALUES ("State Capitals", 1, 0, 2, "All 50 states of the United States and the respective capital cities.", NOW());
+INSERT INTO decks (deck_name, privacy, view_count, user_id, description, created) VALUES ("SAT Words", 1, 0, 3, "SAT prep words and definitions.", NOW());
 
 -- ADD NECESSARY ASSOCIATIONS (for dashboard)
-INSERT INTO my_decks (user_id, deck_id) VALUES (1,1);
-INSERT INTO my_decks (user_id, deck_id) VALUES (2,2);
-INSERT INTO my_decks (user_id, deck_id) VALUES (3,3);
+INSERT INTO my_decks (user_id, deck_id, type, study_count, created) VALUES (1,1,2,0,NOW());
+INSERT INTO my_decks (user_id, deck_id, type, study_count, created) VALUES (1,2,3,0,NOW());
+INSERT INTO my_decks (user_id, deck_id, type, study_count, created) VALUES (1,3,3,0,NOW());
+
+INSERT INTO my_decks (user_id, deck_id, type, study_count, created) VALUES (2,2,2,0,NOW());
+INSERT INTO my_decks (user_id, deck_id, type, study_count, created) VALUES (2,1,3,0,NOW());
+INSERT INTO my_decks (user_id, deck_id, type, study_count, created) VALUES (2,3,3,0,NOW());
+
+INSERT INTO my_decks (user_id, deck_id, type, study_count, created) VALUES (3,3,2,0,NOW());
+INSERT INTO my_decks (user_id, deck_id, type, study_count, created) VALUES (3,1,3,0,NOW());
+INSERT INTO my_decks (user_id, deck_id, type, study_count, created) VALUES (3,1,3,0,NOW());
 
 -- CREATE CARDS
 
