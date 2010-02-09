88d2755913195198e2d6bf2a33582a6ecd3f8c9c
ngiorgi <ngiorgi@56ec8b77-ce45-43ce-8f5e-74b29490009c>
2010-02-09 16:53:23 +0000

    quizcount update

--- db/flashcardsAlterScript.sql
+++ db/flashcardsAlterScript.sql
@@ -2,7 +2,9 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-ALTER TABLE `flashcards`.`cards` DROP COLUMN `order` , ADD COLUMN `card_order` INT(11) NOT NULL  AFTER `id` ;
+ALTER TABLE `flashcards`.`decks` DROP COLUMN `view_count` , ADD COLUMN `quiz_count` INT(11) NOT NULL  AFTER `created` ;
+
+ALTER TABLE `flashcards`.`my_decks` DROP COLUMN `study_count` , ADD COLUMN `quiz_count` INT(11) NOT NULL  AFTER `modified` ;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
