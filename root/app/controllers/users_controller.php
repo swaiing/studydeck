010c253d14cdf54df94af89c1df48ac7518bb438
Stephen Wai <wais@hummel.localdomain>
2011-01-25 20:39:04 -0500

    Fixed bug in paypalIpn callback function which didn't set a flag so that the mydeck association allows the user to learn or quiz.

--- root/app/controllers/users_controller.php
+++ root/app/controllers/users_controller.php
@@ -631,6 +631,7 @@ class UsersController extends AppController {
 					$this->MyDeck->set(array(
 						'user_id' => $user_id,
 						'deck_id' => $product['Product']['deck_id'],
+						'type' => SD_Global::$USER_SAVED
 					));
 					$this->MyDeck->save();					
 					
