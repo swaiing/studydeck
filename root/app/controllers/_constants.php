e96e831a84c4cdad78987ffce07a8f92beaf46bc
Stephen Wai <wais@hummel.localdomain>
2013-05-16 19:21:45 -0400

    Created empty placeholder paypal files, ignoring actual ones

---
+++ root/app/controllers/_constants.php
@@ -0,0 +1,18 @@
+<?php
+
+define("DEFAULT_DEV_CENTRAL", "developer");
+define("DEFAULT_ENV", "sandbox");
+define("DEFAULT_USER_NAME", "");
+define("DEFAULT_PASSWORD", "");
+define("DEFAULT_SIGNATURE", "");
+define("DEFAULT_EMAIL_ADDRESS", "");
+define("DEFAULT_IDENTITY_TOKEN", "");
+define("DEFAULT_EWP_CERT_PATH", "cert/sdk-ewp-cert.pem");
+define("DEFAULT_EWP_PRIVATE_KEY_PATH", "cert/sdk-ewp-key.pem");
+define("DEFAULT_EWP_PRIVATE_KEY_PWD", "password");
+define("DEFAULT_CERT_ID", "");
+define("PAYPAL_CERT_PATH", "cert/sandbox-cert.pem");
+define("BUTTON_IMAGE", "https://www.paypal.com/en_US/i/btn/x-click-but23.gif");
+define("PAYPAL_IPN_LOG", "paypal-ipn.log");
+
+?>
